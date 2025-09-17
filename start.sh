41ce021f7ec2c6737f59df55ac92dd3bf7f51fcd
kylemdonovan <97189054+kylemdonovan@users.noreply.github.com>
2025-09-17 03:20:35 -0400

    TEMP: Add migrate command to start script for one-time update

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 set -e
+
+echo "--- Generating new database migration script on the server ---"
+flask db migrate -m "Add comment replies and likes functionality"
+
+echo "--- Applying all database migrations ---"
 flask db upgrade
+
+echo "--- Starting Gunicorn Server ---"
 gunicorn wsgi:app
