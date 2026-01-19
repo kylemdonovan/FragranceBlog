5eeb692a58ba7eaffa7a9efe7fd5d2cf43867f1c
Kyle Morgan Donovan <97189054+kylemdonovan@users.noreply.github.com>
2026-01-19 23:17:46 +0000

    Security: Implemented Content-Security-Policy and HSTS headers

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,16 @@
 # app/__init__.py
 
 import os
-from flask import Flask
+from flask import Flask, flash, redirect, url_for
 from markupsafe import Markup
-import re # Import the regular expression module
+import re
 from config import Config
 from .extensions import db, migrate, login, csrf, mail, limiter
 import cloudinary
 from flask_wtf.csrf import CSRFError
 from .context_processors import inject_sidebar_data
-from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
-limiter = Limiter(key_func=get_remote_address, default_limits = ["200 per day", "50 per hour"])
+from flask_talisman import Talisman
 
 def create_app(config_class=Config):
     """The application factory."""
@@ -23,7 +22,6 @@ def create_app(config_class=Config):
     except OSError:
         pass
 
-    # Initialize extensions with the app
     db.init_app(app)
     migrate.init_app(app, db)
     login.init_app(app)
@@ -32,27 +30,44 @@ def create_app(config_class=Config):
     limiter.storage_uri = app.config.get('RATELIMIT_STORAGE_URI')    
     limiter.init_app(app)
 
+    csp = {
+        'default-src': "'self'",
+        'script-src': [
+            "'self'",
+            'https://cdn.jsdelivr.net',
+            'https://www.google.com/recaptcha/',
+            'https://www.gstatic.com/recaptcha/',
+            "'unsafe-inline'"
+        ],
+        'style-src': [
+            "'self'",
+            'https://cdn.jsdelivr.net',
+            'https://fonts.googleapis.com',
+            'https://use.fontawesome.com',
+            "'unsafe-inline'"
+        ],
+        'font-src': [
+            "'self'",
+            'https://fonts.gstatic.com',
+            'https://use.fontawesome.com'
+        ],
+        'img-src': ["'self'", 'data:', 'https://res.cloudinary.com']
+    }
+
+    Talisman(app, content_security_policy=csp, force_https=False)
+
     @app.errorhandler(CSRFError)
     def handle_csrf_error(e):
-        # Instead of an ugly error page :(, redirect home with a message (It's coming home)
-        flash("Your session timed out. Please try subscribing again.", "info")
-        return redirect(url_for('main_index'))
-        # --- Custom Highlight Filter ---
+        flash("Your session timed out. Please try again.", "info")
+        return redirect(url_for('main.index'))
+
     def highlight(text, query):
-        """
-        A custom Jinja2 filter to highlight a search query in a block of text.
-        """
         if not query:
             return text
-        # Use case-insensitive REGEX AH!!! to find the query and wrap it in <mark> tags
-        # Markup() is used to prevent Jinja from auto-escaping the HTML blah
         return Markup(re.sub(f'({re.escape(query)})', r'<mark>\1</mark>', text, flags=re.IGNORECASE))
 
-    # Register the custom filter with the Jinja2 environment
     app.jinja_env.filters['highlight'] = highlight
-    # --- END Custom Highlight Filter ---
 
-    # Configure Cloudinary
     if app.config.get('CLOUDINARY_CLOUD_NAME'):
         cloudinary.config(
             cloud_name=app.config['CLOUDINARY_CLOUD_NAME'],
@@ -64,11 +79,8 @@ def create_app(config_class=Config):
     app.context_processor(inject_sidebar_data)
 
     with app.app_context():
-        # Import and register blueprints
         from . import routes
         app.register_blueprint(routes.bp)
-
-        # Import models here so that they are registered with SQLAAAAAAlchemy
         from . import models
 
     return app
